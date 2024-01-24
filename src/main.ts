fc4dc625307644d6b3e7782e9f0e3b59611ca874
DevaNanda <nandadeva870@gmail.com>
2024-01-24 08:22:56 +0700

    bbit changes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import Vue, { createApp, } from 'vue'
+import { createApp, } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,28 +1,5 @@
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
 
-const data = ref();
-
-onMounted(async () => {
-  const response = await (
-    await fetch("http://localhost:8000/api/alat", {
-      headers: {
-        Authorization:
-          "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL2FkbWluL2xvZ2luIiwiaWF0IjoxNzAzNzc1NDc0LCJleHAiOjE3MDM3NzkwNzQsIm5iZiI6MTcwMzc3NTQ3NCwianRpIjoibmt4MzZLUnd2REdQZGxwUSIsInN1YiI6IjUiLCJwcnYiOiJkZjg4M2RiOTdiZDA1ZWY4ZmY4NTA4MmQ2ODZjNDVlODMyZTU5M2E5In0.rSNl6bFShV3-VyUlAbBGzhAfVZ0inaCKV_4JXl9EejU",
-      },
-      method: "POST",
-      body: {
-        kategori_id: 2,
-        nama: "tv",
-        deskripsi: "bagus",
-        hargaperhari: 10000,
-        stok: 4,
-      },
-    })
-  ).json();
-  data.value = await response.data;
-  console.log(await response);
-});
 </script>
 
 <template>
